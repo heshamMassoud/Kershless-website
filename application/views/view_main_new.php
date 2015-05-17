fa58984a61ced489901e15e2d00521576c16e117
Hesham Massoud <hesham.massoud@tum.de>
2015-05-17 19:47:03 +0200

    Change 'register now' button tag to anchor tag

--- application/views/view_main_new.php
+++ application/views/view_main_new.php
@@ -93,9 +93,9 @@
               you win when<br>
               you lose<br>
             </h1>
-            <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+            <a class="btn btn-primary" role="button" href="http://www.kershless.com/register">
               Register Now
-            </button>
+            </a>
           </div>
         </div>
         <div class="item">
