f6dc219d3589a3fc18bca5f1e91c43559e4eedf7
Hesham Massoud <hesham.massoud@tum.de>
2015-07-01 09:56:11 +0200

    Adjust email notification to friend

--- application/models/model_user.php
+++ application/models/model_user.php
@@ -48,68 +48,64 @@ class Model_user extends CI_MODEL
         $this->email->to($friend_email);
         $this->email->subject('Kershless Notification');
         $message = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
-				<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns=\"http://www.w3.org/1999/xhtml\" style=\"width: 100%;\">
-				<head>
-				<!-- Define Charset -->
-				<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
-				<!-- Responsive Meta Tag -->
-				<meta name=\"viewport\" content=\"width=device-width; initial-scale=1.0; maximum-scale=1.0;\" />
-				<!-- Facebook sharing information tags -->
-				<meta property=\"og:title\" content=\"*|MC:SUBJECT|*\" />
-				<title>Kershless Email Notification</title>
-				</head>
-				<body style=\"width: auto !important; text-align: left; background-color: white; margin: 0px; padding: 0px;\" bgcolor=\"white\"><style type=\"text/css\">
-				@font-face { font-family: GE SS Text UltraLight !important; src: url('/kershlesswebsite/Assets/fonts/GE SS Text UltraLight.otf') format(\"opentype\") !important; }
-				&gt;</style>&#13;
-				&#13;
-				<table width=\"100%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" class=\"contentbg\" style=\"background-color: white;\" bgcolor=\"white\"><tr><td valign=\"top\">&#13;
-				<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\"><tr><td class=\"templateColor01\" valign=\"top\" style=\"background-color: #34495e; padding: 0;\" bgcolor=\"#34495e\">&#13;
-				<!-- // Begin Template PreHeader \\ -->&#13;
-				<table class=\"BoxWrap\" width=\"50%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" style=\"width: 280px !important;\"><tr><td height=\"30\">&#13;
-				<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\"><tr><td class=\"text\" mc:edit=\"header_Txt\" style=\"color: white; font-family: 'Arial', sans-serif; font-size: 13px; font-weight: normal; line-height: 18px; text-align: left; text-decoration: none; padding: 10px 0;\" align=\"left\">&#13;
-				<a href=\"#\" style=\"color: white; font-family: 'Arial', sans-serif; font-size: 14px; font-weight: normal; line-height: 18px; text-align: left; text-decoration: none;\">www.kershless.com</a>&#13;
-				</td>&#13;
-				<td class=\"text\" mc:edit=\"header_FindUs\" style=\"text-align: right; color: white; font-family: 'Arial', sans-serif; font-size: 13px; font-weight: normal; line-height: 18px; text-decoration: none;\" align=\"right\">&#13;
-				Find us on:&#13;
-				</td>&#13;
-				<td>&#13;
-				<a href=\"www.fb.com/kershless\" style=\"font-family: 'Arial', sans-serif; font-size: 14px; font-weight: normal; line-height: 18px; color: #77cc33; text-align: left; text-decoration: none;\">&#13;
-				<img align=\"right\" src=\"http://kershless.com/Assets/Icons/social_03.png\" alt=\"icon\" style=\"max-width: 22px; max-height: 22px; outline: none; text-decoration: none; display: block; border: 0px none;\" mc:label=\"image\" mc:edit=\"icon_image03\" /></a>&#13;
-				</td>&#13;
-				<td>&#13;
-				<a href=\"www.twitter.com/kershless\" style=\"font-family: 'Arial', sans-serif; font-size: 14px; font-weight: normal; line-height: 18px; color: #77cc33; text-align: left; text-decoration: none;\">&#13;
-				<img align=\"right\" src=\"http://kershless.com/Assets/Icons/social_02.png\" alt=\"icon\" style=\"max-width: 22px; max-height: 22px; outline: none; text-decoration: none; display: block; border: 0px none;\" mc:label=\"image\" mc:edit=\"icon_image02\" /></a>&#13;
-				</td>&#13;
-				<td>&#13;
-				<a href=\"www.youtube.com/kershless\" style=\"font-family: 'Arial', sans-serif; font-size: 14px; font-weight: normal; line-height: 18px; color: #77cc33; text-align: left; text-decoration: none;\">&#13;
-				<img align=\"right\" src=\"http://kershless.com/Assets/Icons/social_4.png\" alt=\"icon\" style=\"max-width: 22px; max-height: 22px; outline: none; text-decoration: none; display: block; border: 0px none;\" mc:label=\"image\" mc:edit=\"icon_image01\" /></a>&#13;
-				</td>&#13;
-				</tr></table></td>&#13;
-				</tr></table><!-- // End Template PreHeader \\ --></td>&#13;
-				</tr><!-- // ********************************************************************************************************* \\ --><tr><td class=\"templateColor02\" valign=\"bottom\" style=\"background-color: #00A085; padding: 0;\" bgcolor=\"#00A085\">&#13;
-				<!-- // Begin Template Header \\ -->&#13;
-				<table class=\"BoxWrap\" width=\"100%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" style=\"width: 280px !important;\"><tr><td>&#13;
-				<table class=\"BoxWrap\" border=\"0\" width=\"290\" align=\"left\" cellpadding=\"0\" cellspacing=\"0\" style=\"width: 280px !important;\"><tr><td align=\"center\">&#13;
-				</td>&#13;
-				<td class=\"title\" style=\"text-align: center; color: white; font-family: GE SS Text UltraLight; font-size: 24px; line-height: 30px; text-decoration: none;\" align=\"center\"><a href=\"www.kershless.com\" style=\"font-family: 'Arial', sans-serif; font-size: 14px; font-weight: normal; line-height: 18px; color: #77cc33; text-align: left; text-decoration: none;\"><img src=\"http://kershless.com/Assets/Images/small_email.png\" alt=\"logo\" style=\"max-width: 401px; max-height: 194px; outline: none; text-decoration: none; display: block; border: 0px none;\" mc:label=\"image\" mc:edit=\"logo\" /></a>&#13;
-				<br />&#13;
-				عزيزي " . $friend_fullname . "، صديقك " . $applicant_fullname . "<br />(" . $applicant_email . ")
-			&#13;
-				طلب الاشتراك معك في نفس الموسم
-						<hr width=\"250\" /></td>&#13;
-						</tr></table></td>&#13;
-						</tr></table><!-- // End Template Header \\ --></td>&#13;
-						</tr><!-- // ********************************************************************************************************* \\ --><tr><td class=\"templateColor01\" valign=\"top\" style=\"background-color: #34495e; padding: 30px 0px;\" align=\"center\" height=\"100\" mc:edit=\"footer_Copyrights\" bgcolor=\"#34495e\">&#13;
-						<!-- // Begin Module: Footer \\ -->&#13;
-						&#13;
-						<a href=\"#\" class=\"button\" style=\"color: white; font-family: 'Arial', sans-serif; font-size: 11px; font-weight: normal; line-height: 14px; text-align: center; text-decoration: none;\"> Copyright 2014 - All Rights Reserved Kershless</a>			 &#13;
-						<!-- // End Module: Footer \\ -->&#13;
-						</td>&#13;
-						</tr></table></td>&#13;
-						</tr></table><!--Table End--></body>
-						</html>
-						";
-
+                <html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns=\"http://www.w3.org/1999/xhtml\" style=\"width: 100%;\">
+                <head>
+                <!-- Define Charset -->
+                <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
+                <!-- Responsive Meta Tag -->
+                <meta name=\"viewport\" content=\"width=device-width; initial-scale=1.0; maximum-scale=1.0;\" />
+                <!-- Facebook sharing information tags -->
+                <meta property=\"og:title\" content=\"*|MC:SUBJECT|*\" />
+                <title>Kershless Email Notification</title>
+                </head>
+                <body style=\"width: auto !important; text-align: left; background-color: white; margin: 0px; padding: 0px;\" bgcolor=\"white\">
+                &#13;
+                <table width=\"100%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" class=\"contentbg\" style=\"background-color: white;\" bgcolor=\"white\"><tr><td valign=\"top\">&#13;
+                <table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\"><tr><td class=\"templateColor01\" valign=\"top\" style=\"background-color: #34495e; padding: 0;\" bgcolor=\"#34495e\">&#13;
+                <!-- // Begin Template PreHeader \\ -->&#13;
+                <table class=\"BoxWrap\" width=\"50%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" style=\"width: 280px !important;\"><tr><td height=\"30\">&#13;
+                <table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\"><tr><td class=\"text\" mc:edit=\"header_Txt\" style=\"color: white; font-family: 'Arial', sans-serif; font-size: 13px; font-weight: normal; line-height: 18px; text-align: left; text-decoration: none; padding: 10px 0;\" align=\"left\">&#13;
+                <a href=\"#\" style=\"color: white; font-family: 'Arial', sans-serif; font-size: 14px; font-weight: normal; line-height: 18px; text-align: left; text-decoration: none;\">www.kershless.com</a>&#13;
+                </td>&#13;
+                <td class=\"text\" mc:edit=\"header_FindUs\" style=\"text-align: right; color: white; font-family: 'Arial', sans-serif; font-size: 13px; font-weight: normal; line-height: 18px; text-decoration: none;\" align=\"right\">&#13;
+                Find us on:&#13;
+                </td>&#13;
+                <td>&#13;
+                <a href=\"www.fb.com/kershless\" style=\"font-family: 'Arial', sans-serif; font-size: 14px; font-weight: normal; line-height: 18px; color: #77cc33; text-align: left; text-decoration: none;\">&#13;
+                <img align=\"right\" src=\"http://kershless.com/Assets/Icons/social_03.png\" alt=\"icon\" style=\"max-width: 22px; max-height: 22px; outline: none; text-decoration: none; display: block; border: 0px none;\" mc:label=\"image\" mc:edit=\"icon_image03\" /></a>&#13;
+                </td>&#13;
+                <td>&#13;
+                <a href=\"www.twitter.com/kershless\" style=\"font-family: 'Arial', sans-serif; font-size: 14px; font-weight: normal; line-height: 18px; color: #77cc33; text-align: left; text-decoration: none;\">&#13;
+                <img align=\"right\" src=\"http://kershless.com/Assets/Icons/social_02.png\" alt=\"icon\" style=\"max-width: 22px; max-height: 22px; outline: none; text-decoration: none; display: block; border: 0px none;\" mc:label=\"image\" mc:edit=\"icon_image02\" /></a>&#13;
+                </td>&#13;
+                <td>&#13;
+                <a href=\"www.instagram.com/kershless\" style=\"font-family: 'Arial', sans-serif; font-size: 14px; font-weight: normal; line-height: 18px; color: #77cc33; text-align: left; text-decoration: none;\">&#13;
+                <img align=\"right\" src=\"http://kershless.com/Assets/Icons/social_04.png\" alt=\"icon\" style=\"max-width: 22px; max-height: 22px; outline: none; text-decoration: none; display: block; border: 0px none;\" mc:label=\"image\" mc:edit=\"icon_image01\" /></a>&#13;
+                </td>&#13;
+                </tr></table></td>&#13;
+                </tr></table><!-- // End Template PreHeader \\ --></td>&#13;
+                </tr><!-- // ********************************************************************************************************* \\ --><tr><td class=\"templateColor02\" valign=\"bottom\" style=\"background-color: #ffffff; padding: 0;\" bgcolor=\"#00A085\">&#13;
+                <!-- // Begin Template Header \\ -->&#13;
+                <table class=\"BoxWrap\" width=\"100%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" style=\"width: 280px !important;\"><tr><td>&#13;
+                <table class=\"BoxWrap\" border=\"0\" width=\"290\" align=\"left\" cellpadding=\"0\" cellspacing=\"0\" style=\"width: 280px !important;\"><tr><td align=\"center\">&#13;
+                </td>&#13;
+                <td class=\"title\" style=\"text-align: center; line-height: 30px; text-decoration: none;\" align=\"center\"><a href=\"www.kershless.com\" style=\"font-family: 'Arial', sans-serif; text-align: center !important; font-size: 14px; font-weight: normal; line-height: 18px; color: #77cc33; text-decoration: none;\"><img src=\"http://kershless.com/Assets/Images/kershless_190x98.png\" alt=\"logo\" style=\"max-width: 401px; text-align:center !important; max-height: 194px; outline: none; text-decoration: none; display: block; border: 0px none;\" mc:label=\"image\" mc:edit=\"logo\" /></a>&#13;
+                <br />&#13;
+                عزيزي " . $friend_fullname . "، صديقك " . $applicant_fullname . "<br />(" . $applicant_email . ")
+                &#13;
+                طلب الاشتراك معك في نفس الموسم
+                <hr width=\"250\" /></td>&#13;
+                </tr></table></td>&#13;
+                </tr></table><!-- // End Template Header \\ --></td>&#13;
+                </tr><!-- // ********************************************************************************************************* \\ --><tr><td class=\"templateColor01\" valign=\"middle\" style=\"background-color: #34495e !important;\" align=\"center\" height=\"100\" mc:edit=\"footer_Copyrights\" bgcolor=\"#34495e\">&#13;
+                <!-- // Begin Module: Footer \\ -->&#13;
+                &#13;
+                <a href=\"#\" class=\"button\" style=\"color: white ; font-family: 'Arial', sans-serif; font-size: 11px; font-weight: normal; line-height: 14px; text-align: center; text-decoration: none;\"> Copyright 2015 - All Rights Reserved Kershless</a>			 &#13;
+                <!-- // End Module: Footer \\ -->&#13;
+                </td>&#13;
+                </tr></table></td>&#13;
+                </tr></table><!--Table End--></body>
+                </html>";
         $this->email->message($message);
         $this->email->send();
     }
