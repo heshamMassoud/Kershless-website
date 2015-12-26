dfa0dc792da09350a8f3f5eebfb8be8197dec65b
Hesham Massoud <hesham.massoud@commercetools.de>
2015-12-26 23:07:00 +0300

    Add empty common language files

---
+++ application/language/arabic/common_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Sets all common 'Arabic' specific messages.
+ *
+ * PHP version 5
+
+ * @category   PHP
+ * @package    Language
+ * @subpackage Arabic
+ * @author     Hesham Massoud <hesham.massoud@tum.de>
+ * @copyright  2013-2016 Kershless
+ * @license    http://git.io/vtE5Z GNU GENERAL PUBLIC LICENSE
+ * @link       http://www.kershless.com
+ */
+
+//example of placeholders: $lang['welcome'] = "Arabic, %s.";
+
+/* End of file common_lang.php */
+/* Location: application/language/arabic/common_lang.php */
---
+++ application/language/english/common_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Sets all common 'English' specific messages.
+ *
+ * PHP version 5
+
+ * @category   PHP
+ * @package    Language
+ * @subpackage English
+ * @author     Hesham Massoud <hesham.massoud@tum.de>
+ * @copyright  2013-2016 Kershless
+ * @license    http://git.io/vtE5Z GNU GENERAL PUBLIC LICENSE
+ * @link       http://www.kershless.com
+ */
+
+//example of placeholders: $lang['welcome'] = "English, %s.";
+
+/* End of file common_lang.php */
+/* Location: application/language/english/common_lang.php */
