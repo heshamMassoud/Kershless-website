a1640b7a6ce46cc87b5c9084593a3a3447c4b3d3
Hesham Massoud <hesham.massoud@commercetools.de>
2015-12-28 13:10:59 +0300

    Add trim to reason and provocation fields and remove extra line

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -46,7 +46,6 @@ class Register extends CI_Controller
         $this->load->view("view_register");
     }
 
-
     public function rules()
     {
         $this->load->view("view_agreement_terms");
@@ -767,8 +766,8 @@ class Register extends CI_Controller
         $this->form_validation->set_rules('facebook', 'فيسبوك', 'xss_clean|valid_facebook');
         $this->form_validation->set_rules('weight', 'الوزن', 'xss_clean|numeric|greater_than[0]|required');
         $this->form_validation->set_rules('height', 'الطول', 'xss_clean|valid_height|required|numeric|greater_than[0]');
-        $this->form_validation->set_rules('reason', 'الهدف', 'required|xss_clean|arabic');
-        $this->form_validation->set_rules('provocation', 'الكلمة الموجهة للمنافسين', 'required|xss_clean|arabic');
+        $this->form_validation->set_rules('reason', 'الهدف', 'required|trim|xss_clean|arabic');
+        $this->form_validation->set_rules('provocation', 'الكلمة الموجهة للمنافسين', 'required|trim|xss_clean|arabic');
         $this->form_validation->set_rules('friend_code', 'كود التسجيل الخاص بصديقك', 'xss_clean|valid_kershless_code[User.kershless_code]');
         //$this->form_validation->set_rules('name_agreement', 'Agree', 'trim');
         //$this->form_validation->set_rules('picture_agreement', 'Agree', 'trim');
