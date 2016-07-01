e91d43e6fdce10c31cf962c4b286a8ec3673ab7d
Hesham Massoud <hesham.massoud@tum.de>
2016-07-01 21:59:40 +0200

    Close registration

--- application/views/view_main_new.php
+++ application/views/view_main_new.php
@@ -166,8 +166,8 @@ if ($current_lang == 'en') {
     <br>
     <div class="row">
       <div class="text-center register-row">
-        <a role="button" class="btn btn-default register-now" href="register">
-            <?php echo lang('register_now'); ?>
+          <a role="button" class="btn btn-default register-now" onclick="return false;">
+              <?php echo lang('registration_closed'); ?>
         </a>
       </div>
     </div>
