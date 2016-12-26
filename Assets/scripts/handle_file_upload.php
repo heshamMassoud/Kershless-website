97577a3e6debb36a0a58b40369d37f13925d413b
Hesham Massoud <hesham.massoud@tum.de>
2016-12-26 11:11:41 +0100

    Fix uploaded file extensions.

--- Assets/scripts/handle_file_upload.php
+++ Assets/scripts/handle_file_upload.php
@@ -3,53 +3,46 @@ $fileName = time() . $_FILES['file']['name'];
 $fileType = $_FILES['file']['type'];
 $fileContent = file_get_contents($_FILES['file']['tmp_name']);
 $dataUrl = 'data:' . $fileType . ';base64,' . base64_encode($fileContent);
-
-
+$localUploadDirectory = "/Users/heshammassoud/kershless-website/Test/Uploads/";
+$uploadDirectory = "/home4/together/public_html/uploads/";
 $allowedExts = array("gif", "jpeg", "jpg", "png");
-$extension = end(explode(".", $_FILES["file"]["name"]));
+$extension = strtolower(end(explode(".", $_FILES["file"]["name"])));
 
 if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/JPEG")
-|| ($_FILES["file"]["type"] == "image/jpg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/x-png")
-|| ($_FILES["file"]["type"] == "image/png"))
-&& ($_FILES["file"]["size"] < 3000000)
-&& in_array($extension, $allowedExts))
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
-    //echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-	//echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-    }
-  else
-    {
-    //echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-    //echo "Type: " . $_FILES["file"]["type"] . "<br>";
-    //echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    //echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+        || ($_FILES["file"]["type"] == "image/jpeg")
+        || ($_FILES["file"]["type"] == "image/JPEG")
+        || ($_FILES["file"]["type"] == "image/jpg")
+        || ($_FILES["file"]["type"] == "image/JPG")
+        || ($_FILES["file"]["type"] == "image/pjpeg")
+        || ($_FILES["file"]["type"] == "image/x-png")
+        || ($_FILES["file"]["type"] == "image/png"))
+    && ($_FILES["file"]["size"] < 3000000)
+    && in_array($extension, $allowedExts)
+) {
+    if ($_FILES["file"]["error"] > 0) {
+        //echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+        //echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    } else {
+        //echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+        //echo "Type: " . $_FILES["file"]["type"] . "<br>";
+        //echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+        //echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
-    if (file_exists("/home4/together/public_html/uploads/" . $fileName))
-      {
-      //echo $_FILES["file"]["name"] . " already exists. ";
-      }
-    else
-      {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "/home4/together/public_html/uploads/" . $fileName);
-      //echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-      }
+        if (file_exists($localUploadDirectory . $fileName)) {
+            //echo $_FILES["file"]["name"] . " already exists. ";
+        } else {
+            move_uploaded_file($_FILES["file"]["tmp_name"],
+                $localUploadDirectory . $fileName);
+            //echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+        }
     }
-  }
-else
-  {
-  echo "Invalid file";
-  }
+} else {
+    echo "Invalid file";
+}
 $json = json_encode(array(
-  'name' => $fileName,
-  'type' => $fileType,
-  'dataUrl' => $dataUrl
+    'name' => $fileName,
+    'type' => $fileType,
+    'dataUrl' => $dataUrl
 ));
 echo $json;
 ?>
